0507f3ed20dc0590cad9930a7502a6f43a2e6f81
EYATUPE <eyatuphilipesamu@gmail.com>
2023-07-03 14:14:47 -0700

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """A script to calculate a pascal triangle for any number"""
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1] + [triangle[i-1][j] + triangle[i-1][j-1] for j in range(1, i)] + [1]
-        triangle.append(row)
-
+    triangle = []
+    if n > 0:
+        for i in range(n):
+            temp_list = [1 if j == 0 or j == i else triangle[i-1][j-1] + triangle[i-1][j] for j in range(i+1)]
+            triangle.append(temp_list)
     return triangle
