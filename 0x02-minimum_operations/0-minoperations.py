76af7671d45f37abd907f0c6544390c17b0758cc
EYATUPE <eyatuphilipesamu@gmail.com>
2023-07-13 22:08:41 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,18 @@
 def minOperations(n):
     if not isinstance(n, int):
-        return 0  # If n is not an integer, return 0
+        return 0
     
-    ops_count = 0  # Variable to keep track of the number of operations
-    clipboard = 0  # Variable to store the copied value
-    done = 1  # Variable to keep track of the number of 'H' characters achieved
+    ops_count = 0
+    clipboard = 0
+    done = 1
     
     while done < n:
         if n % done == 0:
-            # If the remaining count is divisible by the achieved count, perform Copy All and Paste operation
-            clipboard = done  # Copy the current number of 'H' characters
-            done += clipboard  # Add the copied value to the number of 'H' characters achieved
-            ops_count += 2  # Increment the operation count by 2 (Copy All and Paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
         else:
-            # If the remaining count is not divisible by the achieved count, perform only Paste operation
-            done += clipboard  # Add the copied value to the number of 'H' characters achieved
-            ops_count += 1  # Increment the operation count by 1 (Paste)
+            done += clipboard
+            ops_count += 1
     
-    return ops_count  # Return the total number of operations required
+    return ops_count
